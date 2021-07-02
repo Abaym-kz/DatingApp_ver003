c4e894fa2cf612f10fe4ddeb66075f941e0a250d
abaym <abaym.kz@gmail.com>
2021-07-02 20:26:55 +0600

    End of section 15

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -27,7 +27,8 @@ import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { NgModuleDecorator, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { TextInputComponent } from './_forms/text-input/text-input.component';
-import { DateInputComponent } from './_forms/date-input/date-input.component'
+import { DateInputComponent } from './_forms/date-input/date-input.component';
+import { MemberMessagesComponent } from './members/member-messages/member-messages.component'
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -47,7 +48,8 @@ import { DateInputComponent } from './_forms/date-input/date-input.component'
     MemberEditComponent,
     PhotoEditorComponent,
     TextInputComponent,
-    DateInputComponent
+    DateInputComponent,
+    MemberMessagesComponent
   ],
   imports: [
     BrowserModule,
