f4a41c24a0b18558cf0eda0fa69fb5542eb2080d
abaym <abaym.kz@gmail.com>
2021-07-04 18:20:13 +0600

    End of section 16

---
+++ client/src/app/admin/photo-management/photo-management.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/_models/user';
+import { AdminService } from 'src/app/_services/admin.service';
+
+@Component({
+  selector: 'app-photo-management',
+  templateUrl: './photo-management.component.html',
+  styleUrls: ['./photo-management.component.css']
+})
+export class PhotoManagementComponent implements OnInit {
+  users: Partial<User[]>;
+
+  constructor(private adminService: AdminService) { }
+
+  ngOnInit(): void {
+    this.getUsersWithRoles();
+  }
+
+  getUsersWithRoles() {
+    this.adminService.getUsersWithRoles().subscribe(users => {
+      this.users = users;
+    })
+  }
+
+}
