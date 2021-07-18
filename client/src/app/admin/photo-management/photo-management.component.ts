7b7771fa73e5d90bac47b4323387474c4484cb4d
abaym <abaym.kz@gmail.com>
2021-07-18 13:08:38 +0600

    End of lessaon 247

--- client/src/app/admin/photo-management/photo-management.component.ts
+++ client/src/app/admin/photo-management/photo-management.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/_models/user';
+import { Photo } from 'src/app/_models/photo';
 import { AdminService } from 'src/app/_services/admin.service';
 
 @Component({
@@ -8,17 +8,29 @@ import { AdminService } from 'src/app/_services/admin.service';
   styleUrls: ['./photo-management.component.css']
 })
 export class PhotoManagementComponent implements OnInit {
-  users: Partial<User[]>;
+  photos: Photo[];
 
   constructor(private adminService: AdminService) { }
 
   ngOnInit(): void {
-    this.getUsersWithRoles();
+    this.getPhotosForApproval();
   }
 
-  getUsersWithRoles() {
-    this.adminService.getUsersWithRoles().subscribe(users => {
-      this.users = users;
+  getPhotosForApproval() {
+    this.adminService.getPhotosForApproval().subscribe(photos => {
+      this.photos = photos;
+    })
+  }
+
+  approvePhoto(photoId) {
+    this.adminService.approvePhoto(photoId).subscribe(() => {
+      this.photos.splice(this.photos.findIndex(p => p.id === photoId), 1);
+    })
+  }
+
+  rejectPhoto(photoId) {
+    this.adminService.rejectPhoto(photoId).subscribe(() => {
+      this.photos.splice(this.photos.findIndex(p => p.id === photoId, 1));
     })
   }
 
